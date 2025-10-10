2e2993b90dc42756e16cb2ddab8a5807ed593e86
Mykhailo Kirpa <mk@smth.smth>
2025-10-10 13:23:17 +0200

    Start

---
+++ WypozyczalniaSamochodow/WypozyczalniaSamochodow.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using WypozyczalniaSamochodow.Model;
+
+FirstClass fc = new FirstClass();
+
+Person p1 = new Person() { FirstName = "Igor", LastName="Borowicz", Age=21 };
+Console.WriteLine(p1);
+//Console.WriteLine($"{p1.FirstName} {p1.LastName} {p1.Age}");
