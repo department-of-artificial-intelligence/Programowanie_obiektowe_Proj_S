325ae24d8b35b56c148263071e0408b88787920e
Stanislav Ivantsov <SI@smth.smth>
2025-10-10 13:32:07 +0200

    Start

--- Project/Project.ConsoleApp/Program.cs
+++ Project/Project.ConsoleApp/Program.cs
@@ -3,6 +3,7 @@
 using Project.Model;
 
 
+
 Person p1 = new Person() { FirstName = "Jan", LastName = "Kowalski", Age = 40 };
 Console.WriteLine(p1);
 //Console.WriteLine($"{p1.FirstName} {p1.LastName} {p1.Age}");
