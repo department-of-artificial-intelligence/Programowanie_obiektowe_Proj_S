f647486be9ffe8cae609d3821f601dbf63b8486f
Piotr Nowak <piotrnowak@smth.smth>
2025-10-10 13:20:56 +0200

    proj lab 1

---
+++ Biura.model/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biura.model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} {Age}";
+        }
+    }
+}
