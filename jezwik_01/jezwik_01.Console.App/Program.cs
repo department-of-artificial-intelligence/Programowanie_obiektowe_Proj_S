35fadafbc18a61febf8290f5ddefe449f30d95d7
Vladyslav Turchynovych <jsbfu@gmail.com>
2025-10-10 13:24:53 +0200

    start

---
+++ jezwik_01/jezwik_01.Console.App/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using jezwik_01.Model;
+using System.IO.Pipes;
+using System.Net.Cache;
+//1_klasa 
+
+Person p1 = new Person() { FirstName = "Jan" , LastName = "Kowalski" , Age = 40 };
+
+
+
+Console.WriteLine($"{p1.FirstName} {p1.LastName} {p1.Age}");
+Console.WriteLine(p1);
---
+++ jezwik_01/jezwik_01.Model/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jezwik_01.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }        
+        public required int Age { get; set; }
+
+
+
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} {Age}";
+        }
+    }
+}
