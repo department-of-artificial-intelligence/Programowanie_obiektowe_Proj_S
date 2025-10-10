25dd14102f56b16a1932956bfb6a943fdbe7007c
Piotr Nowak <piotrnowak@smth.smth>
2025-10-10 13:25:57 +0200

    start

--- Biura.ConsoleApp/Program.cs
+++ Biura.ConsoleApp/Program.cs
@@ -2,6 +2,8 @@
 //Console.WriteLine("Hello, World!");
 using Biura.model;
 
-
 PierwszaKlasa pk = new PierwszaKlasa();
 
+Person p1 = new Person() { FirstName = "Jan", LastName = "Kowalski" };
+
+Console.WriteLine(p1);
