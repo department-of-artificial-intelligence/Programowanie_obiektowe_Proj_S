f2050d5fc617a8e8cba4b77a817d9dcfd9f7bef5
Adam Kowal <asd@asd.sad>
2025-10-10 13:42:46 +0200

    First Flass

---
+++ Project/Project.Model/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int RegNum { get; set; }
+        public int ProdYear { get; set; }
+        public int MileAge { get; set; } //przebieg
+        public string Name { get; set; }
+
+    }
+}
