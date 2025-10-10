49ec123450d5475b5392704d3b5080cbe6ee36bf
Stanislav Ivantsov <SI@smth.smth>
2025-10-10 13:21:45 +0200

    Start

---
+++ Project/Project.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Project.Model;
+
+
+
+
+Person p1 = new Person() { FirstName = "Jan", LastName = "Kowalski", Age = 40 };
+Console.WriteLine(p1);
+//Console.WriteLine($"{p1.FirstName} {p1.LastName} {p1.Age}");
