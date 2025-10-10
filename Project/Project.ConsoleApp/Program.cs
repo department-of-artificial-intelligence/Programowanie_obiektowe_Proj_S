4de3a16a2292f19d7e06341617edc7da3965943e
Oskar Turek <ot@smth.smth>
2025-10-10 13:21:10 +0200

    Start

---
+++ Project/Project.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Project.Model;
+
+PierwszaKlasa pk = new PierwszaKlasa();
+
+Person p1 = new Person() { FirstName = "Jan", LastName = "Kowalski", Age = 40 };
+
+Console.WriteLine(p1);
+//Console.WriteLine($"{p1.FirstName} {p1.LastName} {p1.Age}");
