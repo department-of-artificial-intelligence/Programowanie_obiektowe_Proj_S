9f6ac97fad01711f4119de321c205c725ffd15d0
Student <st@st>
2025-10-10 13:25:13 +0200

    Start

---
+++ Lab1/Lab1.ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Project.Model;
+using System.Globalization;
+Pierwsza pk = new Pierwsza();
+
+Person p1 = new Person() {FirstName = "Jan", LastName="PP", Age=10};
+Console.WriteLine($"{p1.FirstName} {p1.LastName} {p1.Age}" );
---
+++ Lab1/Project.Model/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} {Age}";
+        }
+
+    }
+}
