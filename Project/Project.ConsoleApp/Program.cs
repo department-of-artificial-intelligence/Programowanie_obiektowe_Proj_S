7b867ccd56e4d4b809dd46de04b7d3a1d6e27061
Adam Kowal <asd@asd.sad>
2025-10-10 13:31:27 +0200

    Start

---
+++ Project/Project.ConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿using Project.Model;
+PierwszaKlasa pk = new PierwszaKlasa();
+
+Person p1 = new Project.Model.Person() { FirstName = "Jan", LastName = "Kowalski", Age = 40 };
+Console.WriteLine($"{p1.FirstName} {p1.LastName} {p1.Age}");
+
