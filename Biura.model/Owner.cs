599c1f95ffadc1912daf002cce2ed5e60efe5722
Piotr Nowak <piotrnowak@smth.smth>
2025-10-10 13:44:47 +0200

    koniec

---
+++ Biura.model/Owner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biura.model
+{
+    internal class Owner
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Biura.model/biuro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biura.model
+{
+    
+    internal class biuro
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public Owner Owner { get; set; }
+        
+
+    }
+}
