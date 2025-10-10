9b9f78071b87118e7013829fb71fa2684e16ff6f
Student <Student@LAB-SK-1-2>
2025-10-10 13:21:16 +0200

    Start

---
+++ Project/Project.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Project.ConsoleApp
+{
+
+    using Project.Model;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PierwszaKlasa klas = new PierwszaKlasa();
+
+            Person person = new Person() { FirstName = "Jan", LastName = "Kowalski", Age = 11 };
+
+            person.Age = 12;
+
+            //Console.WriteLine($"{person.LastName}");
+
+            //Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(person);
+        }
+    }
+}
