2c7c03be6b73e43f37b09794fa639c32ab9acfab
Michal Zultaszek <yel@yel.yel>
2025-10-10 13:21:07 +0200

    Start

---
+++ CarRental/CarRental.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using CarRental.Model;
+
+PierwszaKlasa pk = new PierwszaKlasa();
+
+Person p1 = new Person() { FirstName = "Jan", LastName = "Kowalski", Age = 34 };
+
+Console.WriteLine(p1);
+//Console.WriteLine($"{p1.FirstName} {p1.LastName} {p1.Age}");
