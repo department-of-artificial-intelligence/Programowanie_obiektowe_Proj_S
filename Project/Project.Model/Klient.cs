fa5b2f8ed51b26bf8a73a3945f1c91cddc7f7c7d
filip <zqwcjyu@gmail.com>
2025-10-14 18:22:36 +0200

    n1

--- Project/Project.Model/Klient.cs
+++ Project/Project.Model/Klient.cs
@@ -10,7 +10,7 @@ namespace Project.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Adress { get; set; }
+        public string Address { get; set; }
 
 
     }
---
+++ Project/Project.Model/Venue.cs
@@ -0,0 +1,9 @@
+namespace Project.Model;
+
+public class Venue
+{
+    public int Id { get; set; }
+    public int Size {get; set;}
+    public string Address {get; set;}
+    
+}
