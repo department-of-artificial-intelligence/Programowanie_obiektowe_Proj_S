cf6cbbfac5f7c47903b5b49b85dd3e5ce3abaf6c
Student <st@st>
2025-10-17 12:29:54 +0200

    pupupu

--- Lab1/Lab1.ConsoleApp/Program.cs
+++ Lab1/Lab1.ConsoleApp/Program.cs
@@ -5,4 +5,5 @@ using System.Globalization;
 Pierwsza pk = new Pierwsza();
 
 Person p1 = new Person() {FirstName = "Jan", LastName="PP", Age=10};
+
 Console.WriteLine($"{p1.FirstName} {p1.LastName} {p1.Age}" );
