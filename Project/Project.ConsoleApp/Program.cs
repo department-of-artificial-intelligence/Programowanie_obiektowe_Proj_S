ee2165f265b24777ac8c84da19a481d24f46b422
Student <Student@LAB514-16>
2025-10-22 10:03:37 +0200

    Start

--- Project/Project.ConsoleApp/Program.cs
+++ Project/Project.ConsoleApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Project.Model;
+//using Project.Model;
+//Person person = new Person() { FirstName = "Jan", LastName = "Kowalski", PhoneNumber = "123456789" };
 
-Person person = new Person() { FirstName = "Jan", LastName = "Kowalski", PhoneNumber = "123456789" };
-
+Console.WriteLine("Hello world");
