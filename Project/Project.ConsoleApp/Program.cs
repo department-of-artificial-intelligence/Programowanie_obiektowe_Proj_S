b39341b5b51ef52c0dcb38edddb3a0ebcf887578
Student <Student@LAB514-2>
2025-10-22 09:44:58 +0200

    AAAAAAAAA

--- Project/Project.ConsoleApp/Program.cs
+++ Project/Project.ConsoleApp/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//using Project.Model;
+//Restaurant restaurant = new Restaurant();
---
+++ Project/Project.Model/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model
+{
+    public class Restaurant
+    {
+        public required string Name {  get; set; }
+        public required string Address { get; set; }
+        public List<MenuItem> Menu { get; set; } = new();
+        public List<Employee> Employees { get; set; } = new();
+
+        public Restaurant(string name, string address) {
+            Name = name;
+            Address = address;
+            Menu = new List<MenuItem>();
+            Employees = new List<Employee>();
+        }
+    }
+}
