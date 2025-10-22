2d6dcd13beb6262e94cf0ba9fd9cc7eb102f4f4f
Student <Student@LAB>
2025-10-22 09:44:34 +0200

    Szkic1

--- Projekt/Projekt.Model/Class1.cs
+++ Projekt/Projekt.Model/Class1.cs
@@ -1,7 +1,19 @@
-﻿namespace Projekt.Model
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Model
 {
-    public class Class1
+    public class Auto
     {
+
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Przebieg { get; set; }
+        public float Silnik { get; set; }
+        public int Rocznik { get; set; }
 
     }
 }
