39659274151fda8c53e65769be911b7acbec579c
Student <Student@LAB514-3>
2025-10-22 09:45:16 +0200

    AAA

--- Project/Project.ConsoleApp/Program.cs
+++ Project/Project.ConsoleApp/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+using Project.Model;
+Hotel hotel = new hotel();
+
---
+++ Project/Project.Model/Hotel.cs
@@ -0,0 +1,11 @@
+﻿namespace Project.Model
+{
+    public class Hotel
+    {
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+
+        public Hotel() { }
+
+    }
+}
