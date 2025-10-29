ae0a76418693372a56d65c0baca5e141676a2afe
Student <Student@LAB514-4>
2025-10-29 09:40:42 +0100

    "test"

--- Project.Model/Class1.cs
+++ Project.Model/Class1.cs
@@ -1,7 +1,13 @@
-﻿namespace Project.Model
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model
 {
     public class Class1
     {
-
+        
     }
 }
