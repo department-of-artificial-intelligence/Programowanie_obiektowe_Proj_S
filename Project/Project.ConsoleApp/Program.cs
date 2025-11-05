ef43a894bacce35a774cebf6ee7f20768180eb3f
FormalOdin81 Ciura <adrian.ciura@onet.eu>
2025-11-05 09:37:50 +0100

    vvvvvvv

--- Project/Project.ConsoleApp/Program.cs
+++ Project/Project.ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ public class Trenerzy
     public required string Age { get; set; }
     public required string Email { get; set; }
     public required string Phone { get; set; }
+    ///jjj
   
    
 
