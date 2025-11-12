de9925bf9418bbaca81e5400385b89107719343d
Student <Student@LAB514-13>
2025-11-12 09:45:10 +0100

    Created MenuItem class

--- Project/Project.Model/MenuItem.cs
+++ Project/Project.Model/MenuItem.cs
@@ -1,8 +1,28 @@
 ﻿using System;
 
-public class MenuItems
+public class MenuItem
 {
-	public MenuItems()
+	public string? Name { get; set; }
+	public string? Description { get; set; }
+	public decimal? Price { get; set; }
+
+	public MenuItem() : this(String.Empty, String.Empty, 0) { }
+
+	public MenuItem(string name, string description, decimal price)
 	{
+		Name = name;
+		Description = description;
+		Price = price;
 	}
+
+	public void SetPrice(decimal price)
+	{
+		Price = price;
+	}
+
+	public void GetInfo()
+	{
+		Console.WriteLine($"Name: {Name}\nDesctription: {Description}\nPrice: {Price}");
+	}
+
 }
