eeb8c04bbcfcf566b3d771c31ed6d3066a528097
rklymiuk <klymrom05@gmail.com>
2025-11-16 22:09:51 +0100

    first steps

---
+++ RatingSystem/Domain/Rating.cs
@@ -0,0 +1,43 @@
+namespace RatingSystem.Domain;
+
+public class Rating
+{
+    private int _id;
+    private int _serviceId;
+    private int _value;
+    private string _comment;
+
+    public int Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public int ServiceId
+    {
+        get => _serviceId;
+        set => _serviceId = value;
+    }
+
+    public int Value
+    {
+        get => _value;
+        set => _value = value;
+    }
+
+    public string Comment
+    {
+        get => _comment;
+        set => _comment = value;
+    }
+
+    Rating(int id, int serviceId, int value, string comment)
+    {
+        _id = id;
+        _serviceId = serviceId;
+        _value = value;
+        _comment = comment;
+    }
+    Rating():this(0, 0, 0, string.Empty){}
+    
+}
