74916ac958051c2fcb5f42b42f1b074fa469690c
Student <ST@ST>
2025-11-21 13:06:08 +0100

    XUnit test init

---
+++ Project/Project.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace Project.Tests
+{
+    public class PersonUnitTests
+    {
+        [Fact]
+
+        // public void test(a1, a1) - parametry; Test(a1, a2) argumenty, [Fact] - attrybuty(dekoratory)
+        // public void CreatePersonTest()
+        //{
+        //    IPersonServise personService = new PersonService();
+        //    Person p1 = personService.CreatePerson("", "");
+
+        //    throw new Exception();
+
+        //    Assert.Null(p1.Description);
+        //    Assert.NotNull(p1.Description);
+        //    Assert.NotEmpty(p1.Description);
+        //    Assert.True(p1.Description);
+        //}
+    }
+}
