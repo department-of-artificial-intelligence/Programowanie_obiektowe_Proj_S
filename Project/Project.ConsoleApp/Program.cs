a7cb01f3cf6526904a59239e0cf324d3e01709bc
Vitalikmylian <vitalikarhitektor@gmail.com>
2025-11-28 11:54:27 +0100

    tests

--- Project/Project.ConsoleApp/Program.cs
+++ Project/Project.ConsoleApp/Program.cs
@@ -1,5 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿
 using Project.Model;
 
+namespace Project.ConsoleApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
 
+            
+        }
+    }
+}
---
+++ Project/Project.Model/RentalTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project.Model;
+using System.Collections.Generic;
+
+namespace Project.Model.Tests
+{
+    [TestClass]
+    public class RentalTests
+    {
+        private Station _station;
+        private Customer _customer;
+        private Employee _manager;
+        private Bicycle _bike;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _station = new Station("Central Park Station", 20);
+
+            _bike = new Bicycle { Id = 101, Model = "Giant Escape", Type = "City", Price = 15 };
+            _station.AddBicycle(_bike);
+
+            _customer = new Customer("John", "Doe", "5th Avenue, NY");
+            _manager = new Employee("Alice", "Smith", 1, EmployeeRole.Manager);
+        }
+
+        [TestMethod]
+        public void RentBike_SuccessfulRental_UpdatesStatusAndCount()
+        {
+            _manager.ProcessRental(_customer, _bike);
+
+            Assert.AreEqual(BicycleStatus.Rented, _bike.Status, "Bike status should change to Rented.");
+            Assert.AreEqual(1, _customer.BicycleCount, "Customer should have 1 bike.");
+        }
+
+        [TestMethod]
+        public void RentBike_LimitReached_DeniesRental()
+        {
+            _customer.AddBicycle(new Bicycle { Id = 1 });
+            _customer.AddBicycle(new Bicycle { Id = 2 });
+            _customer.AddBicycle(new Bicycle { Id = 3 });
+
+            Bicycle extraBike = new Bicycle { Id = 999 };
+
+            _manager.ProcessRental(_customer, extraBike);
+
+            Assert.AreEqual(3, _customer.BicycleCount, "Customer cannot rent more than 3 bikes.");
+            Assert.AreEqual(BicycleStatus.Available, extraBike.Status, "Bike should remain Available.");
+        }
+    }
+}
