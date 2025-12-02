8a4135cff19be855a529372368f8589be0993ab5
Mykhailo Lytvyn <smth@smth.smth>
2025-12-02 23:09:28 +0100

    +Worker

--- Project/Project.ConsoleApp/Program.cs
+++ Project/Project.ConsoleApp/Program.cs
@@ -5,15 +5,8 @@ public class Program
 {
     public static void Main()
     {
-        var pizzeria = new Pizzeria("Pizza Tower", "Dąbrowskiego");
+        var worker = new Worker("Mykhailo", "Lytvyn", 6500, "Cook");
 
-        pizzeria.Menu.AddItem(new MenuItem(1, "Margherita", 25, "Classic"));
-        pizzeria.Menu.AddItem(new MenuItem(2, "Pepperoni", 30, "Spicy"));
-
-        var client = new Client(10, "John", "Doe", "555-123");
-        var order = pizzeria.PlaceOrder(client, new List<string> { "Margherita", "Pepperoni" });
-
-        Console.WriteLine(order.GetInfo());
-        Console.WriteLine(pizzeria.GetInfo());
+        Console.WriteLine(worker.GetInfo());
     }
 }
--- Project/Project.Model/Worker.cs
+++ Project/Project.Model/Worker.cs
@@ -1,8 +1,25 @@
 ﻿using System;
+using Project.Model;
 
-public class Worker
+public class Worker : Person, IShowInfo
 {
-	public Worker()
-	{
-	}
+    public decimal Salary { get; set; }
+    public string Position { get; set; }
+
+    public Worker() : this(string.Empty, string.Empty, 0, string.Empty) { }
+    public Worker(string firstName, string lastName, decimal salary, string position) : base(firstName, lastName)
+    {
+        Salary = salary;
+        Position = position;
+    }
+
+    public string GetInfo()
+    {
+        var info = $"---- Worker {FirstName} {LastName}----\n";
+        info += $"Salary: {Salary}\n";
+        info += $"Position: {Position}\n";
+        info += "-------------------------";
+
+        return info;
+    }
 }
