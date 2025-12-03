a0d06d28da6b81f4386a6e2e872b23d5696907eb
Mykhailo Lytvyn <smth@smth.smth>
2025-12-03 22:31:14 +0100

    And pizzeriasnetwork

--- Project/Project.Model/Pizzerias.cs
+++ Project/Project.Model/Pizzerias.cs
@@ -1,8 +1,63 @@
 ﻿using System;
+using System.Xml.Linq;
+using Project.Model;
 
-public class Pizzerias
+public class PizzeriasNetwork
 {
-	public Pizzerias()
-	{
-	}
+    public IList<Pizzeria> PizzeriasList { get; set; }
+
+    public PizzeriasNetwork()
+    {
+        PizzeriasList = new List<Pizzeria>();
+    }
+
+    public void AddPizzeria(Pizzeria pizzeria)
+    {
+        if (pizzeria == null) return;
+
+        if (PizzeriasList.Any(p => p.Name == pizzeria.Name))
+        {
+            Console.WriteLine($"Error: Pizzeria '{pizzeria.Name}' already exists in the network.");
+            return;
+        }
+
+        PizzeriasList.Add(pizzeria);
+    }
+
+    public void RemovePizzeria(string name)
+    {
+        var pizzeriaToRemove = GetPizzeria(name);
+
+        if (pizzeriaToRemove != null)
+        {
+            PizzeriasList.Remove(pizzeriaToRemove);
+        }
+        else
+        {
+            Console.WriteLine($"Error: Could not find pizzeria '{name}'.");
+        }
+    }
+
+    public Pizzeria? GetPizzeria(string name)
+    {
+        return PizzeriasList.FirstOrDefault(p => p.Name == name);
+    }
+
+    public void DisplayAll()
+    {
+        Console.WriteLine("\n================ NETWORK OVERVIEW ================");
+        if (PizzeriasList.Count == 0)
+        {
+            Console.WriteLine("No pizzerias currently in the network.");
+        }
+        else
+        {
+            foreach (var pizzeria in PizzeriasList)
+            {
+                Console.WriteLine(pizzeria.GetInfo());
+                Console.WriteLine();
+            }
+        }
+        Console.WriteLine("==================================================");
+    }
 }
