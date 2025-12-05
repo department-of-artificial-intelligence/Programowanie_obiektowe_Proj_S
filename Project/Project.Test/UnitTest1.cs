fb63b25aead607f2e6720cb28b0e490826ca9ecb
jan kowlaczyk <kami@dere.pl>
2025-12-05 12:51:44 +0100

    test test

---
+++ Project/Project.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace Project.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
+
+// testowanie jednostkowe mamy robic sssss
