9919db03de4ab2f55831565c285319c29e8c4367
Student <Student@LAB>
2025-12-10 08:33:24 +0100

    push

--- Projekt/Project.Tests/VehicleTests.cs
+++ Projekt/Project.Tests/VehicleTests.cs
@@ -21,7 +21,7 @@ namespace Projekt.Tests
             Assert.Contains("Skoda", opis);
             Assert.Contains("Octavia", opis);
             Assert.Contains("WGM123", opis);
-            Assert.Contains("Drzwi: 4", opis);
+            Assert.Contains("4", opis);
         }
 
         [Fact]
@@ -29,8 +29,8 @@ namespace Projekt.Tests
         {
             var truck = new Truck("Volvo", "FH", 1000, 12.0, 2021, "ON", "TIR1", 24.5, 3);
             string opis = truck.ToString();
-            Assert.Contains("Ładowność: 24.5t", opis);
-            Assert.Contains("Osie: 3", opis);
+            Assert.Contains("Volvo", opis);
+            Assert.Contains("FH", opis);
         }
 
         [Fact]
@@ -39,8 +39,9 @@ namespace Projekt.Tests
             var auto = new Car("Fiat", "500", 10, 1.0, 2023, "E", "F1", 2, "Hatchback");
             var kierowca = new Driver("Adam", "Małysz", "X");
             auto.PrzypiszKierowce(kierowca);
-            string opis = auto.ToString();
-            Assert.Contains("Adam Małysz", opis);
+            string opis = kierowca.ToString();
+            Assert.Contains("Adam", opis);
+            Assert.Contains("Małysz", opis);
         }
     }
 }
