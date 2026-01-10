2143bd587903226f37d75ca7c46e1cce32e38ffd
Aleksander_Nafalski <aleksander.nafalski.pl@gmail.com>
2026-01-10 19:48:51 +0100

    Edit102604

--- Project/Project.Test/StoreUnitTest.cs
+++ Project/Project.Test/StoreUnitTest.cs
@@ -11,28 +11,28 @@ namespace Project.Test
     public class StoreTestUnit
     {
         [Fact]
-        public void StoreTest() // Nazwa metody z Twojego logu błędów
+        public void StoreTest() 
         {
-            // --- ARRANGE ---
+            
             var addressWarsaw = new Address("Warszawa", "Marszałkowska", "00-001", "Polska");
-            // Numer sklepu jest OK (+48...)
+            
             var store1 = new Store("SuperMarkecik", addressWarsaw, "+48732304650");
 
             var addressCracow = new Address("Kraków", "Floriańska", "31-019", "Polska");
             var store2 = new Store("Delikatesy Centrum", addressCracow, "+48123456789");
 
-            // POPRAWKA 1: Prawidłowy numer telefonu dla Managera
+            
             var manager = new Employee(
                 "Adam", "Nowak",
-                "+48123456789", // <-- Było "123", musi być pełny format
+                "+48123456789", 
                 "a@a.pl",
                 store1, EmployeePosition.Manager, 6000m, new DateTime(2022, 1, 10));
             store1.Staff.Add(manager);
 
-            // POPRAWKA 2: Prawidłowy numer telefonu dla Senior Sprzedawcy
+            
             var seniorSeller = new Employee(
                 "Piotr", "Zielinski",
-                "+48666777888", // <-- Było "555", musi być pełny format
+                "+48666777888",
                 "p@p.pl",
                 store2, EmployeePosition.Manager, 4800m, new DateTime(2020, 5, 15));
             store2.Staff.Add(seniorSeller);
@@ -45,15 +45,15 @@ namespace Project.Test
             store1.Inventory.AddRange(new[] { p1, p2 });
             store2.Inventory.AddRange(new[] { p3, p4, p2 });
 
-            // POPRAWKA 3: Prawidłowy numer telefonu dla Klienta
+            
             var customer = new Customer(
                 "Ewa", "Kowalska",
-                "+48987654321", // <-- Było z myślnikami, a Twój regex może ich nie lubić
+                "+48987654321",
                 "ewa@klient.pl");
 
             var deliveryAddress = new Address("Warszawa", "ul. Polna 5", "00-123", "Polska");
 
-            // --- ACT ---
+            
             var order1 = new Order(customer, deliveryAddress);
             order1.AddProduct(p1, 10);
             order1.AddProduct(p2, 2);
@@ -72,7 +72,7 @@ namespace Project.Test
             {
                 if (o.Status == OrderStatus.Shipped)
                 {
-                    // Używamy metody z nawiasami ()
+                    
                     totalSpent += o.GetTotalAmount();
                 }
             }
