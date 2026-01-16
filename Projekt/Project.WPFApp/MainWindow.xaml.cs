0e6b13e56a2b7925f6169e3241d18b835bf6f9d7
Bohdan Ferents <email@email.com>
2026-01-16 13:42:04 +0100

    WPF i MVVM część 1

--- Projekt/Project.WPFApp/MainWindow.xaml.cs
+++ Projekt/Project.WPFApp/MainWindow.xaml.cs
@@ -20,5 +20,43 @@ namespace Project.WPFApp
         {
             InitializeComponent();
         }
+
+        /*/
+
+        private void ButtPokaz(object sender, RoutedEventArgs e)
+        {
+            var okno = new OknoListaPrac();
+            okno.ShowDialog();
+        }
+
+        private void ButtZatrudnij(object sender, RoutedEventArgs e)
+        {
+            var okno = new OknoZatrudnij();
+            okno.ShowDialog();
+        }
+
+        private void ButtWyplaty(object sender, RoutedEventArgs e)
+        {
+            var okno = new OknoWyplaty();
+            okno.ShowDialog();
+        }
+
+        private void ButtNajlepszy(object sender, RoutedEventArgs e)
+        {
+            var okno = new OknoNajlepszy();
+            okno.ShowDialog();
+        }
+
+        private void ButtZwolnij(object sender, RoutedEventArgs e)
+        {
+            var okno = new OknoZwolnij();
+            okno.ShowDialog();
+        }
+
+        private void ButtWyjscie(object sender, RoutedEventArgs e)
+        {
+
+        }
+       /*/
     }
 }
